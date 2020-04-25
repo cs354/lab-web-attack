e84fd71495613658e5f2896ff25b8dde60351909
n8ta <n8@u.northwestern.edu>
2020-04-25 15:00:43 -0500

    fix shebang, validate port

--- web-attack-lab.bash
+++ web-attack-lab.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 ## student-env container name is : attacker-id
 ## lab container name is web-attack-lab-id
@@ -35,7 +35,7 @@ start_attacker() {
   echo " "
   if [ $id = local ]
   then
-    echo "    The site is available to attack @ http://localhost:5000"
+    echo "   The site is available to attack @ http://localhost:5000"
   else
     echo "   To make the server available on your local machine (not on vicious) run :"
     echo "   ssh -L 5000:localhost:${port} ${id}@vicious.cs.northwestern.edu"
@@ -64,11 +64,17 @@ fi
 
 if [ $id = local ]
 then
+  echo "LOCAL"
   docker run --rm -d --name web-attack-lab-${id} -p 5000:5000 --network web-attack-lab-${id} cs354/web-attack-lab:latest
 else
-  echo "Enter the port number of vicious you want to use, remember this port #"
-  read;
-  port=${REPLY}
+  read_port=0
+  while (( read_port < 1000 || read_port >  65535))
+  do
+    echo "Enter the port number of vicious you want to use (1000 -> 65535), remember this port #"
+    read;
+    read_port=${REPLY}
+  done
+  port=$read_port
   docker run --rm -d --name web-attack-lab-${id} -p ${port}:5000 --network web-attack-lab-${id} cs354/web-attack-lab:latest
 fi
 
